0adf3aa8a66d5e1200cca90a940dcf64ea6bf3a6
wKur3k <kurek.wojciech.01@gmail.com>
2024-02-15 17:07:17 +0100

    Initial commit

---
+++ OnePieceTCGResultsApi/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnePieceTCGResultsApi.Entities;
+
+namespace OnePieceTCGResultsApi.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddValidators(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ OnePieceTCGResultsApi/Entities/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnePieceTCGResultsApi.Entities;
+
+public class AppDbContext : DbContext
+{
+    protected readonly IConfiguration _configuration;
+
+    public AppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("ApiDbConnectionString"));
+    }
+}
