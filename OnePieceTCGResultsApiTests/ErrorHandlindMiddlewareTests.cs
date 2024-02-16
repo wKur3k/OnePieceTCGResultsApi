96a76ab1c38a306b300b3aa39f33fe0b434f39ea
wKur3k <kurek.wojciech.01@gmail.com>
2024-02-16 11:05:59 +0100

    Unit test init

---
+++ OnePieceTCGResultsApiTests/ErrorHandlindMiddlewareTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using OnePieceTCGResultsApi.Exceptions;
+using OnePieceTCGResultsApi.Middleware;
+
+namespace OnePieceTCGResultsApiTests;
+
+public class ErrorHandlindMiddlewareTests
+{
+    [Fact]
+    public async Task InvokeAsync_CatchBadRequestException_ReturnsBadRequest()
+    {
+        // Arrange
+        var middleware = new ErrorHandlingMiddleware();
+        var context = new DefaultHttpContext();
+        var nextDelegateMock = new Mock<RequestDelegate>();
+        nextDelegateMock.Setup(x => x.Invoke(It.IsAny<HttpContext>())).Throws(new BadRequestException("Bad request"));
+
+        // Act
+        await middleware.InvokeAsync(context, nextDelegateMock.Object);
+
+        // Assert
+        Assert.Equal((int)HttpStatusCode.BadRequest, context.Response.StatusCode);
+        Assert.Equal("application/json", context.Response.ContentType);
+    }
+
+    [Fact]
+    public async Task InvokeAsync_CatchGeneralException_ReturnsInternalServerError()
+    {
+        // Arrange
+        var middleware = new ErrorHandlingMiddleware();
+        var context = new DefaultHttpContext();
+        var nextDelegateMock = new Mock<RequestDelegate>();
+        nextDelegateMock.Setup(x => x.Invoke(It.IsAny<HttpContext>())).Throws(new Exception("Internal server error"));
+
+        // Act
+        await middleware.InvokeAsync(context, nextDelegateMock.Object);
+
+        // Assert
+        Assert.Equal((int)HttpStatusCode.InternalServerError, context.Response.StatusCode);
+        Assert.Equal("application/json", context.Response.ContentType);
+    }
+}
